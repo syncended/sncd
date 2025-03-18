b7cc108571ac8564175b41e857936d9e1cf224a2
syncended <syncended@gmail.com>
2025-03-19 02:07:06 +0300

    add table model

---
+++ src/main/kotlin/dev/syncended/sncd/model/table/UrlTable.kt
@@ -0,0 +1,11 @@
+package dev.syncended.sncd.model.table
+
+import org.jetbrains.exposed.sql.Table
+
+object UrlTable : Table() {
+    val id = integer(name = "id").autoIncrement()
+    val secretKey = integer(name = "secret_key")
+    val url = text(name = "url")
+
+    override val primaryKey: PrimaryKey? get() = PrimaryKey(id)
+}
