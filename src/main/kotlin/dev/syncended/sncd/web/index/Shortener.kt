01816ccb922ffa7d18d50504b3231b55b12a40bc
syncended <syncended@gmail.com>
2025-03-24 00:26:15 +0300

    add url selectable

--- src/main/kotlin/dev/syncended/sncd/web/index/Shortener.kt
+++ src/main/kotlin/dev/syncended/sncd/web/index/Shortener.kt
@@ -4,16 +4,24 @@ import dev.syncended.kube.components.ui.ButtonType
 import dev.syncended.kube.core.component.Layout
 import dev.syncended.kube.core.component.Modifier
 import dev.syncended.kube.core.component.fillMaxWidth
+import dev.syncended.kube.core.component.marginRight
 import dev.syncended.kube.core.component.marginTop
-import dev.syncended.kube.core.model.RenderMode
+import dev.syncended.kube.core.component.textSelection
+import dev.syncended.kube.core.model.Alignment
+import dev.syncended.kube.core.model.TextSelection
+import dev.syncended.kube.dsl.box
 import dev.syncended.kube.dsl.buttonInput
+import dev.syncended.kube.dsl.column
 import dev.syncended.kube.dsl.form
+import dev.syncended.kube.dsl.row
+import dev.syncended.kube.dsl.space
 import dev.syncended.kube.dsl.text
 import dev.syncended.kube.dsl.textInput
 import dev.syncended.kube.htmx.modifier.hxPost
-import dev.syncended.kube.ktor.core.respondRender
+import dev.syncended.kube.ktor.core.respondRenderView
 import dev.syncended.kube.styling.Colors
 import dev.syncended.kube.styling.Size.rem05
+import dev.syncended.kube.styling.Size.rem1
 import dev.syncended.sncd.app.inject
 import dev.syncended.sncd.model.FormData
 import dev.syncended.sncd.service.operation.CreateShortUrlOperation
@@ -28,7 +36,7 @@ fun Routing.postShortener() = post("/shortener") {
     val url = formInput[FormData.URL]
     val operationResult = createShortUrlOperation(url)
 
-    call.respondRender(mode = RenderMode.VIEW_ONLY) {
+    call.respondRenderView {
         operationResult
             .onSuccess { shortUrl(it) }
             .onFailure { shortenerInput(it.toString()) }
@@ -53,6 +61,9 @@ fun Layout.shortenerInput(error: String? = null) {
     }
 }
 
-fun Layout.shortUrl(url: String) {
-    text("Your short url is: $url")
+fun Layout.shortUrl(url: String) = column(modifier = Modifier.fillMaxWidth(), alignment = Alignment.Horizontal.Center) {
+    row {
+        text("Short url:", modifier = Modifier.marginRight(rem1))
+        text(url, modifier = Modifier.textSelection(TextSelection.ALL))
+    }
 }
